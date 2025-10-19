a7f59d6d27912c49dbb35d2c1c248d365d5a7e22
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2025-10-19 15:31:29 +0800

    feat: add response models and dependencies for API serialization

---
+++ shared/src/response_models.rs
@@ -0,0 +1,15 @@
+use domain::models::Resume;
+use rocket::serde::Serialize;
+
+#[derive(Serialize)]
+pub enum ResponseBody {
+    Message(String),
+    Resume(Resume),
+    Resumes(Vec<Resume>)
+}
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct Response {
+    pub body: ResponseBody,
+}
