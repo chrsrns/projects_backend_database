e3e63649f78749ff845fa7bae725ab9b852cbeb4
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2025-12-13 16:49:11 +0800

    refactor: extract rocket configuration to lib
    - Moved rocket build logic from main.rs to new build_rocket() function in lib.rs
    - Simplified main.rs to call build_rocket() directly

--- api/src/bin/main.rs
+++ api/src/bin/main.rs
@@ -1,17 +1,9 @@
 #[macro_use]
 extern crate rocket;
-use api::resume_handler;
+
+use api;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount(
-        "/api",
-        routes![
-            resume_handler::list_resumes_handler,
-            resume_handler::list_resume_handler,
-            resume_handler::create_resume_handler,
-            resume_handler::update_resume_handler,
-            resume_handler::delete_resume_handler,
-        ],
-    )
+    api::build_rocket()
 }
--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,1 +1,17 @@
+#[macro_use]
+extern crate rocket;
+
 pub mod resume_handler;
+
+pub fn build_rocket() -> rocket::Rocket<rocket::Build> {
+    rocket::build().mount(
+        "/api",
+        routes![
+            resume_handler::list_resumes_handler,
+            resume_handler::list_resume_handler,
+            resume_handler::create_resume_handler,
+            resume_handler::update_resume_handler,
+            resume_handler::delete_resume_handler,
+        ],
+    )
+}
