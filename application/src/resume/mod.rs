50d52089edaf6d95c2268e57531a9595ad402c08
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2026-03-04 13:43:23 +0800

    refactor: reorganize resume module imports for better readability

--- application/src/resume/mod.rs
+++ application/src/resume/mod.rs
@@ -1,12 +1,13 @@
 pub mod create;
 pub mod delete;
+pub mod read;
+pub mod update;
+
 pub mod education;
 pub mod frameworks;
 pub mod languages;
 pub mod portfolio_projects;
-pub mod read;
 pub mod skills;
-pub mod update;
 pub mod work_experiences;
 
 pub mod common;
